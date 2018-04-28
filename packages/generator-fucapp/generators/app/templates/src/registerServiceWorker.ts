4b330f009393f3fcce5d69cc4ca97b1775dedb53
wolfstark <>
2018-04-28 10:59:54 +0800

    feat($location(tsx)):

--- packages/generator-fucapp/generators/app/templates/src/registerServiceWorker.ts
+++ packages/generator-fucapp/generators/app/templates/src/registerServiceWorker.ts
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log('App is being served from cache by a service worker.\n' +
-          'For more details, visit https://goo.gl/AFskqB');
+        'For more details, visit https://goo.gl/AFskqB');
     },
     cached() {
       console.log('Content has been cached for offline use.');
--- packages/generator-fucapp/generators/app/templates/src/typings/shims.d.ts
+++ packages/generator-fucapp/generators/app/templates/src/typings/shims.d.ts
@@ -1,4 +1,17 @@
-declare module "*.vue" {
-  import Vue from "vue";
+import Vue, { VNode } from 'vue';
+
+declare module '*.vue' {
   export default Vue;
+}
+
+declare global {
+  namespace JSX {
+    // tslint:disable no-empty-interface
+    interface Element extends VNode {}
+    // tslint:disable no-empty-interface
+    interface ElementClass extends Vue {}
+    interface IntrinsicElements {
+      [elem: string]: any
+    }
+  }
 }
