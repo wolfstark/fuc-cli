617be5f246132e8c7abec55cd7d480f1ad776eba
wenxi <1038999405@qq.com>
2018-10-11 15:09:39 +0800

    feat:

--- packages/generator-fucapp/generators/app/templates/.eslintrc.js
+++ packages/generator-fucapp/generators/app/templates/.eslintrc.js
@@ -5,11 +5,12 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@fuc/app',
+    '@vue/airbnb'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
